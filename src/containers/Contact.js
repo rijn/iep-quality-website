221dca1ce00298509fdfd84d5f47e4d9d010ef1b
Rijn <bxbian951122@gmail.com>
2018-03-06 13:43:45 -0600

    Add google maps

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 
+import { compose, withProps } from "recompose"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+
+const MapComponent = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.30&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props =>
+  <GoogleMap
+    defaultZoom={15}
+    defaultCenter={{ lat: 40.397, lng: -88.644 }}
+  >
+  </GoogleMap>
+);
+
 class Contact extends Component {
   render() {
     return (
       <div>
         <h1>Contact</h1>
+        <MapComponent />
       </div>
     );
   }
